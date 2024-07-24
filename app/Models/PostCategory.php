492490439fe77f774b4ca77f823ded1e40b5c2dc
Yasuhiro WATANABE <info@nabeyasu.com>
2024-07-24 23:17:05 +0900

    [Hiro/init/make_files] - make initial project files
    Additions:
    - Added Controllers
    - Added Models
    - Added Migrations

---
+++ app/Models/PostCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostCategory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'post_category'; // table name is singular
+    protected $fillable = ['post_id', 'category_id'];
+
+    
+}
