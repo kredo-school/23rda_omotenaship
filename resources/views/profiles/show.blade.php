16dafd4fde96313ccc1b1d0add5d53b7145e3b19
DESKTOP-KOGVC0R\ii5aa <awawanov05@gmail.com>
2024-07-25 22:24:47 +0900

    Merge branch 'main' into Akane/FE/Profile_show.

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,74 +1,49 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Profile 
-</body>
-</html> -->
-
 @extends('layouts.app')
 
 @section('content')
 
-<!-- <link rel="stylesheet" href="{{ asset('css/profile.css') }}"> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+/* style　>　profiles-show.css */
+    <title>Profile</title>
 
-<div class="profile-container">
-    <div class="background-image" style="background-image: url('{{ asset('images/profile/profileback.jpg') }}');"></div>
-    <div class="content">
-        <div class="card mx-auto w-50" style="min-width: 480px; min-height: 450px;">
-            <div class="card-body text-center">
-                <div class="position-absolute top-0 end-0 mt-2 me-5">
-                    <div class="dropdown custom-dropdown-container">
-                        <button class="btn btn-outline-light btn-sm custom-dropdown" type="button" id="dropdownPro" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="fs-1 text-dark">&#8230;</span>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownPro">
-                            <li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#user-profile-edit"><i class="fa-solid fa-pen-to-square"></i>&nbsp; Edit</a></li>
-                            <li><a class="dropdown-item text-danger" href="#" data-bs-toggle="modal" data-bs-target="#user-profile-delete"><i class="fa-solid fa-trash-can"></i>&nbsp; Delete</a></li>
-                        </ul>
-                    </div>
-                </div>
-                @include('users.profile.modal.delete')
-                @include('users.profile.modal.edit')
+</head>
 
-                <div class="text-center">
-                    <i class="fa-solid fa-circle-user fa-8x mb-2"></i>
-                    <h1 class="card-title">John</h1>
-                    <p class="text-muted">John Smith</p>
-                </div>
+<body>
 
-                <div class="row mb-2">
-                    <div class="col-6">
-                        <div class="content">
-                            <a href="#" class="text-decoration-none">
-                                <div class="card custom-bg border border-0 custom-card">
-                                    <div class="card-body">
-                                        <p class="card-text">Events</p>
-                                        <h5 class="card-title"><i class="fa-solid fa-list"></i>&nbsp; 24</h5>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="content">
-                            <a href="#" class="text-decoration-none">
-                                <div class="card custom-bg border border-0 custom-card">
-                                    <div class="card-body">
-                                        <p class="card-text">Comments</p>
-                                        <h5 class="card-title"><i class="fa-regular fa-comment"></i>&nbsp; 5</h5>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+    <div class="profile-container">
+        <div class="profile-side">
+            <div class="column">
+                <i class="fa-solid fa-user text-dark"></i>
+                <img 
+                    src="profile-photo.jpg" 
+                    alt="John Adam Smith" 
+                    class="profile-image">
+                <h1
+                    class="profile-name">
+                    John Adam Smith
+                </h1>
+                <p class="profile-bio">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <!-- Add more profile information here -->
             </div>
         </div>
+            <div class="profile-my-posts">
+                <i class="fa-solid fa-user text-dark"></i>
+                <img 
+                    src="profile-photo.jpg" 
+                    alt="John Adam Smith" 
+                    class="profile-image">
+                <h1
+                    class="profile-name">
+                    John Adam Smith
+                </h1>
+                <p class="profile-bio">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <!-- Add more profile information here -->
+            </div>
     </div>
-</div>
+</body>
+</html>
 @endsection
