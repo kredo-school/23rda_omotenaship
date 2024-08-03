14280b03cc26ca41a6059e7a2c29b5a00baec0b1
Yasuhiro WATANABE <info@nabeyasu.com>
2024-08-03 15:40:51 +0900

    [Hiro/FE/posts_index]  - Comment out unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,19 @@ use App\Http\Controllers\AdminNgwordController;
 Route::get('/', [PostController::class, 'index'])
     ->name('index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-
+// Route for auth
 require __DIR__ . '/auth.php';
+
 // posts
 Route::group(['prefix' => 'posts', 'as' => 'posts.'], function () {
     Route::get('/create', [PostController::class, 'create'])->name('create');
