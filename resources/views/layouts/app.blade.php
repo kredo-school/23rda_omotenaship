b81b88e61887dcda7c1d2241f1c99121f97dac33
Ruri Nogami <ruuuusan.1128@gmail.com>
2024-08-03 12:56:12 +0900

    Merge branch 'Ruri/FE/posts_show' of github.com:kredo-school/23rda_omotenaship into Ruri/FE/posts_show
    # Conflicts:
    #	resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,28 +19,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-<<<<<<<<< Temporary merge branch 1
-        <!-- CSS -->
-       <link rel="stylesheet" href="{{ asset('css/admin-posts-index.css') }}">
-       <link rel="stylesheet" href="{{ asset('css/post-create.css') }}">
-       <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-       <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/profiles.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/posts-show.css') }}">
-=========
->>>>>>>>> Temporary merge branch 2
-
-
+        
     <!-- Bootstrap        -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
-<<<<<<<<< Temporary merge branch 1
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-           
-    </body>
-=========
+        
     <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('css/admin-posts-index.css') }}">
     <link rel="stylesheet" href="{{ asset('css/post-create.css') }}">
@@ -66,5 +50,4 @@
 
 </body>
 
->>>>>>>>> Temporary merge branch 2
 </html>
