d3238998871e23893c2db86e4ef2358027959f20
Yasuhiro WATANABE <info@nabeyasu.com>
2024-08-08 21:09:18 +0900

    [Hiro/BE/register] - fix register function

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,12 +3,15 @@
 @section('title', 'Register')
 
 @section('content')
+    {{-- Navbar --}}
     @include('components.navbar')
+
     <div class="row">
         <!-- left -->
         <div class="col-lg-3">
             <img src="{{ asset('images/logos/blue5.png') }}" alt="bluecat" class="bluecat">
         </div>
+
         <!-- center -->
         <div class="card border-0 col-lg-6 mt-3 d-flex align-items-center justify-content-center">
             <img src="{{ asset('images/logos/red5.png') }}" alt="redcat" class="redcat">
@@ -18,11 +21,12 @@
 
                 <form method="POST" action="{{ route('register') }}" class="mt-4">
                     @csrf
+
                     <!-- username -->
                     <div class="row mb-3">
                         <label for="username" class="col-md-3 col-form-label text-black">{{ __('Username') }}</label>
                         <div class="col-md-6">
-                            <input id="username" type="text" class="form-control bg-transparent"name="username"
+                            <input type="text" class="form-control bg-transparent" id="username" name="username"
                                 value="{{ old('username') }}" required autocomplete="username" autofocus>
 
                             @error('username')
@@ -37,8 +41,8 @@
                     <div class="row mb-3">
                         <label for="password" class="col-md-3 col-form-label text-black">{{ __('Password') }}</label>
                         <div class="col-md-6">
-                            <input id="username" type="text" class="form-control bg-transparent"name="password"
-                                value="{{ old('password') }}" required autocomplete="password" autofocus>
+                            <input type="password" class="form-control bg-transparent" id="password" name="password"
+                                value="{{ old('password') }}" required autocomplete="password">
 
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -50,37 +54,41 @@
 
                     <!-- Comfirm password -->
                     <div class="row mb-3">
-                        <label for="comfirmpassword"
+                        <label for="password_confirmation"
                             class="col-md-3 col-form-label text-black">{{ __('Comfirm Password') }}</label>
                         <div class="col-md-6">
-                            <input id="comfirmpassword" type="text"
-                                class="form-control bg-transparent"name="comfirmpassword"
-                                value="{{ old('comfirmpassword') }}" required autocomplete="comfirmpassword" autofocus>
+                            <input type="password" class="form-control bg-transparent" id="password_confirmation"
+                                name="password_confirmation" value="" required>
 
-                            @error('comfirmpassword')
+                            @error('password_confirmation')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </div>
                     </div>
-                    <!-- login -->
+
+                    <!-- Register Button -->
                     <div class="row mb-3">
                         <div class="d-flex justify-content-center">
                             <button type="submit" class="btn btn-kurenai w-50">
-                                Register</button>
+                                Register
+                            </button>
                         </div>
+
+                        {{-- Link to login page --}}
                         <a href={{ route('login') }} class="text-black text-center">Alredy have an account? Login</a>
                     </div>
                 </form>
             </div>
         </div>
+
         <!-- right -->
         <div class="col-lg-3 p-0">
             <img src="{{ asset('/images/logos/blue5.png') }}" alt="bluecat" class="bluecat">
         </div>
     </div>
 
+    {{-- Footer --}}
     @include('components.footer')
-
 @endsection
