bc2ecea56c4050db802f228394d49d1b8635081a
Ruri Nogami <ruuuusan.1128@gmail.com>
2024-08-10 11:19:02 +0900

    adjusted position

---
+++ resources/views/browsing-history/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('title', 'Browsing-history')
+
+@section('content')
+
+    @include('components.navbar')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-auto">
+
+                {{-- heading --}}
+                <h2 class="mb-3"><span class="px-2 heading-kurenai text-bold">Browsing History</span></h2>
+
+                {{-- Posts --}}
+                <div class="row">
+                    <div class="col mb-3">
+                        @include('components.post')
+                    </div>
+                    <div class="col mb-3">
+                        @include('components.post')
+                    </div>
+                    <div class="col mb-3">
+                        @include('components.post')
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col mb-3">
+                        @include('components.post')
+                    </div>
+                    <div class="col mb-3">
+                        @include('components.post')
+                    </div>
+                    <div class="col mb-3">
+                        @include('components.post')
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    {{-- Footer --}}
+    @include('components.footer')
+
+@endsection
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,17 +5,18 @@
 @section('content')
     {{-- @include('components.navbar') --}}
     <div class="container">
-        {{-- <div class="row justify-content-center d-flex"> --}}
-            {{-- <div class="col-8 mx-auto"> --}}
-                <div class="row">
-                    <h2 class="heading-kurenai"><span>New Post</span></h2>
-                </div>
+        <div class="row justify-content-center d-flex">
+            <div class="col-7">
+
+                {{-- heading --}}
+                <h2 class="heading-kurenai"><span>New Post</span></h2>
+
                 <form action="#" method="post" enctype="multipart/form-data">
                     @csrf
 
                     {{-- category --}}
                     <div class="row">
-                        <div class="col-8 mb-4">
+                        <div class="col mb-4">
                             <label for="category" class="form-label d-block fw-bold">
                                 Category
                             </label>
@@ -36,7 +37,7 @@
                     </div>
                     {{-- title --}}
                     <div class="row">
-                        <div class="col-8 mb-4">
+                        <div class="col mb-4">
                             <label for="title" class="form-label fw-bold">Title</label>
                             <textarea name="title" id="title" class="form-control">{{ old('title') }}</textarea>
                             <!-- Error -->
@@ -47,7 +48,7 @@
                     </div>
                     {{-- artical --}}
                     <div class="row">
-                        <div class="col-8 mb-4">
+                        <div class="col mb-4">
                             <label for="article" class="form-label fw-bold">Article</label>
                             <textarea name="article" id="article" rows="3" class="form-control">{{ old('article') }}</textarea>
                             <!-- Error -->
@@ -58,7 +59,7 @@
                     </div>
                     {{-- images --}}
                     <div class="row">
-                        <div class="col-8 mb-4">
+                        <div class="col mb-4">
                             <label for="image" class="form-label fw-bold">Image</label>
                             <input type="file" name="image" id="image" class="form-control"
                                 aria-describedby="image-info">
@@ -67,99 +68,102 @@
                                 <p class="mb-0">Maximum file size is 1048kb.</p>
                             </div>
                             @error('image')
-                                <div class="text-danger small">{{ $message }}</div>
+                                <div class="text-danger small">{{ $message }}
+                                </div>
                             @enderror
                         </div>
+                        </row>
                         {{-- caption --}}
-                        <div class="col-8 mb-4">
-                            <label for="caption" class="form-label fw-bold">Caption</label>
-                            <textarea name="caption" id="caption" class="form-control">{{ old('caption') }}</textarea>
-                            <!-- Error -->
-                            @error('caption')
-                                <div class="text-danger small">{{ $message }}</div>
-                            @enderror
+                        <div class="row">
+                            <div class="col mb-4">
+                                <label for="caption" class="form-label fw-bold">Caption</label>
+                                <textarea name="caption" id="caption" class="form-control">{{ old('caption') }}</textarea>
+                                <!-- Error -->
+                                @error('caption')
+                                    <div class="text-danger small">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    {{-- Date --}}
-                    <div class="row">
-                        <div class="col-8 mb-4">
-                            <label for="Date of visit" class="form-label fw-bold">Date of visit</label>
-                            <br>
-                            <label class="date-edit">
-                                <input type="date" name="visit_date" class="rounded-2">
-                            </label>
-                            <!-- Error -->
-                            @error('date')
-                                <div class="text-danger small">{{ $message }}</div>
-                            @enderror
+                        {{-- Date --}}
+                        <div class="row">
+                            <div class="col mb-4">
+                                <label for="Date of visit" class="form-label fw-bold">Date of visit</label>
+                                <br>
+                                <label class="date-edit">
+                                    <input type="date" name="visit_date" class="rounded-2">
+                                </label>
+                                <!-- Error -->
+                                @error('date')
+                                    <div class="text-danger small">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    {{-- Area of japan --}}
-                    <div class="row">
-                        <div class="col-8 mb-4">
-                            <label for="Area of Japan" class="form-label fw-bold">Area of Japan</label>
-                            <select class="form-select form-select-lg mb-3" name="area_id" id="area">
-                                {{-- @foreach ($all_areas as $area)
+                        {{-- Area of japan --}}
+                        <div class="row">
+                            <div class="col mb-4">
+                                <label for="Area of Japan" class="form-label fw-bold">Area of Japan</label>
+                                <select class="form-select form-select-lg mb-3" name="area_id" id="area">
+                                    {{-- @foreach ($all_areas as $area)
                                         <option value="{{ $area->id }}">{{ $area->name }}</option>
                                     @endforeach --}}
-                            </select>
+                                </select>
 
-                            <!-- Error -->
-                            @error('date')
-                                <div class="text-danger small">{{ $message }}</div>
-                            @enderror
+                                <!-- Error -->
+                                @error('date')
+                                    <div class="text-danger small">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    {{-- prefecture --}}
-                    <div class="row">
-                        <div class="col-8 mb-4">
-                            <label for="Prefecture of Japan" class="form-label fw-bold">Prefecture of Japan</label>
-                            <select class="form-select form-select-lg mb-3" name="prefecture_id" id="prefecture">
-                                {{-- @foreach ($all_prefectures as $prefecture)
+                        {{-- prefecture --}}
+                        <div class="row">
+                            <div class="col mb-4">
+                                <label for="Prefecture of Japan" class="form-label fw-bold">Prefecture of Japan</label>
+                                <select class="form-select form-select-lg mb-3" name="prefecture_id" id="prefecture">
+                                    {{-- @foreach ($all_prefectures as $prefecture)
                                         <option value="{{ $prefecture->id }}">{{ $prefecture->name }}</option>
                                     @endforeach --}}
-                            </select>
+                                </select>
 
-                            <!-- Error -->
-                            @error('date')
-                                <div class="text-danger small">{{ $message }}</div>
-                            @enderror
+                                <!-- Error -->
+                                @error('date')
+                                    <div class="text-danger small">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    {{-- Event --}}
-                    <div class="row">
-                        <div class="col-8 mb-4">
-                            <label for="event of date" class="form-label fw-bold">Event of date</label>
-                            <br>
-                            <label class="date-edit">
-                                <span>Start</span>
-                                <input type="date" class="rounded-2" name="start_date">
-                            </label>
-                            <br>
-                            <br>
-                            <label class="date-edit">
-                                <span> End </span>
-                                <input type="date" class="rounded-2" name="end_date">
-                            </label>
-                            <!-- Error -->
-                            @error('date')
-                                <div class="text-danger small">{{ $message }}</div>
-                            @enderror
+                        {{-- Event --}}
+                        <div class="row">
+                            <div class="col mb-4">
+                                <label for="event of date" class="form-label fw-bold">Event of date</label>
+                                <br>
+                                <label class="date-edit">
+                                    <span>Start</span>
+                                    <input type="date" class="rounded-2" name="start_date">
+                                </label>
+                                <br>
+                                <br>
+                                <label class="date-edit">
+                                    <span> End </span>
+                                    <input type="date" class="rounded-2" name="end_date">
+                                </label>
+                                <!-- Error -->
+                                @error('date')
+                                    <div class="text-danger small">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
 
-                    {{-- Button --}}
-                    <div class="row">
-                        <div class="mb-4 col-4">
-                            <button type="submit"
-                                class="btn post-button-kurenai btn-lg px-5 text-white btn-kurenai btn-kurenai:hover"
-                                id="post-button">Post
-                            </button>
+                        {{-- Button --}}
+                        <div class="row">
+                            <div class="mb-4 col-6 mx-auto d-grid gap-2">
+                                <button type="submit"
+                                    class="btn post-button-kurenai btn-lg px-5 text-white btn-kurenai btn-kurenai:hover"
+                                    id="post-button">Post
+                                </button>
+                            </div>
                         </div>
-                    </div>
                 </form>
-            {{-- </div> --}}
-        {{-- </div> --}}
+            </div>
+        </div>
     </div>
 
     {{-- @include('components.footer') --}}
