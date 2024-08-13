49dda7588e75cee4afaa742a3ec1e087d5ed9431
RIKO MAEZAWA <>
2024-08-13 21:18:53 +0900

    edit seeder

--- database/seeders/ProfilesTableSeeder.php
+++ database/seeders/ProfilesTableSeeder.php
@@ -18,6 +18,27 @@ class ProfilesTableSeeder extends Seeder
      */
     public function run(): void
     {
+        // Get image path
+        $img_path1 = public_path('images/seeders/avatars/Avatar_user_id1.jpg');
+        $img_path2 = public_path('images/seeders/avatars/Avatar_user_id2.jpg');
+        $img_path3 = public_path('images/seeders/avatars/Avatar_user_id3.jpg');
+        $img_path4 = public_path('images/seeders/avatars/Avatar_user_id4.jpg');
+        $img_path5 = public_path('images/seeders/avatars/Avatar_user_id5.jpg');
+
+        // Get image content from path
+        $img_contents1 = file_get_contents($img_path1);
+        $img_contents2 = file_get_contents($img_path2);
+        $img_contents3 = file_get_contents($img_path3);
+        $img_contents4 = file_get_contents($img_path4);
+        $img_contents5 = file_get_contents($img_path5);
+
+        // convert image into uri
+        $data_uri1 = $this->generateDataUri($img_contents1, $img_path1);
+        $data_uri2 = $this->generateDataUri($img_contents2, $img_path2);
+        $data_uri3 = $this->generateDataUri($img_contents3, $img_path3);
+        $data_uri4 = $this->generateDataUri($img_contents4, $img_path4);
+        $data_uri5 = $this->generateDataUri($img_contents5, $img_path5);
+
         $profiles = [
             [
                 'user_id' => '1',
@@ -31,7 +52,7 @@ class ProfilesTableSeeder extends Seeder
                 Interests: Technology, visiting cafes, historical buildings, local cuisine
                 
                 I am interested in new technology and café culture, and look forward to checking out the latest tech trends in the various places I visit during my travels. He is also passionate about finding cafes that are unique to the area and trying the local coffee and sweets. When visiting historical buildings, I learn about the architectural styles and the historical context in which they were built, while documenting my travels by leaving photos and sketches. When it comes to local cuisine, they reflect on their trips by learning about popular local ingredients and cooking methods and trying to recreate them at home.I hope you all enjoy Japan as much as I did!',
-                'avatar' => 'public/images/seeders/avatars/Avatar_user_id1.jpg',
+                'avatar' => "$data_uri1",
                 'birth_date' => '1994-02-02',
                 'language' => 'en-US',
                 'created_at' => NOW(),
@@ -45,7 +66,7 @@ class ProfilesTableSeeder extends Seeder
                 'introduction' => 'I am a fashion blogger by trade✨.
                 I cover the latest fashions and trendy spots in Japan and share them on my blog and social networking sites. I am interested in fashion, shopping, art and music festivals and often travel with a group of friends.
                 I am active in urban areas of Japan, especially Tokyo and Osaka! My Japanese is at a beginner level (lol).',
-                'avatar' => 'public/images/seeders/avatars/Avatar_user_id2.jpg',
+                'avatar' => "$data_uri2",
                 'birth_date' => '1988-04-12',
                 'language' => 'en-US',
                 'created_at' => NOW(),
@@ -58,7 +79,7 @@ class ProfilesTableSeeder extends Seeder
                 'middle_name' => 'Austin',
                 'introduction' => 'I enjoy traveling with my wife and two children. I can speak a little Japanese!
                 I would like to learn about Japanese food culture! I would like to get inspiration from Japanese cuisine to introduce new menu items in my own restaurant.',
-                'avatar' => 'public/images/seeders/avatars/Avatar_user_id3.jpg',
+                'avatar' => "$data_uri3",
                 'birth_date' => '1997-06-22',
                 'language' => 'en-US',
                 'created_at' => NOW(),
@@ -75,7 +96,7 @@ class ProfilesTableSeeder extends Seeder
                 I would like to provide information on inexpensive accommodations and public transportation!
                 Traveling to sacred places of Japanese anime and experiencing traditional culture is a wonderful experience!
                 Please do not hesitate to contact me! ',
-                'avatar' => 'public/images/seeders/avatars/Avatar_user_id4.jpg',
+                'avatar' => "$data_uri4",
                 'birth_date' => '1996-08-12',
                 'language' => 'en-US',
                 'created_at' => NOW(),
@@ -88,7 +109,7 @@ class ProfilesTableSeeder extends Seeder
                 'middle_name' => '',
                 'introduction' => 'I am a retired businessman and now enjoy luxury travel.
                 I would love to relax at a luxury resort.',
-                'avatar' => 'public/images/seeders/avatars/Avatar_user_id5.jpg',
+                'avatar' => "$data_uri5",
                 'birth_date' => '1970-10-22',
                 'language' => 'en-US',
                 'created_at' => NOW(),
@@ -97,5 +118,19 @@ class ProfilesTableSeeder extends Seeder
         ];
 
         $this->profile->insert($profiles);
+    }
+
+    private function generateDataUri($img_contents, $img_path)
+    {
+        // Get file extension
+        $img_extension = pathinfo($img_path, PATHINFO_EXTENSION);
+
+        // convert image into base64 data
+        $base64_img = base64_encode($img_contents);
+
+        // compose data uri
+        $data_uri = 'data:image/' . $img_extension . ';base64,' . $base64_img;
+
+        return $data_uri;
     }
 }
