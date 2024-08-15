70599f7fa2a9f52a62369a010571a56acd3593ee
Yasuhiro WATANABE <info@nabeyasu.com>
2024-08-16 00:40:14 +0900

    [Hiro/BE/login] - fix view for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,12 +26,12 @@
 
                     <!-- username -->
                     <div class="row mb-3">
-                        <label for="username" class="col-md-3 col-form-label text-black">{{ __('Username') }}</label>
+                        <label for="name" class="col-md-3 col-form-label text-black">{{ __('Username') }}</label>
                         <div class="col-md-6">
-                            <input type="text" class="form-control bg-transparent" id="username" name="username"
-                                value="{{ old('username') }}" required autocomplete="username" autofocus>
+                            <input type="text" class="form-control bg-transparent" id="name" name="name"
+                                value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                            @error('username')
+                            @error('name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
