eadfbb851b81d93797bcdc0306315054d3d43cd2
Hiromi Okajima <mee.oremoka.may@gmail.com>
2024-08-17 10:34:08 +0900

    Hiromi/BE/Favorite_index edit page

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -2,13 +2,21 @@
     {{-- <div class="card-img-top"></div> --}}
     {{-- <div class="row"> --}}
     <img src="{{ asset('images/14.png') }}" class="img-fluid card-img-top">
-    <img src="{{ asset('images/Star1.png') }}" class="star-icon">
+    <input type="hidden" name="user_id" value=2>
+    <input type="hidden" name="user_id" value=1>
+
+    <button type="submit">
+        <i class="fa-solid fa-star text-warning star-icon"></i>
+    </button>
     {{-- </div> --}}
     <div class="card-body">
         <div class="row">
             <div class="col-10">
                 <p class="mb-0 fs-5">title flower garden</p>
-                <p class="mb-0"><span class="rounded-1 mr-2 border border-dark px-1">Review</span><span class="rounded-1 mr-2 border border-dark px-1">Event</span><span class="rounded-1 mr-2 border border-dark px-1">Volunteer</span><span class="rounded-1 mr-2 border border-dark px-1">Culture</span></p>
+                <p class="mb-0"><span class="rounded-1 mr-2 border border-dark px-1">Review</span><span
+                        class="rounded-1 mr-2 border border-dark px-1">Event</span><span
+                        class="rounded-1 mr-2 border border-dark px-1">Volunteer</span><span
+                        class="rounded-1 mr-2 border border-dark px-1">Culture</span></p>
                 <p>Username</p>
             </div>
             <div class="col-2 mt-4 pt-3">
