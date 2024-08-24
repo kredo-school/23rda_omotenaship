96785d54020ac23929e375a51ea84b7fc3ac2fcd
DESKTOP-KOGVC0R\ii5aa <awawanov05@gmail.com>
2024-08-24 14:44:00 +0900

    Akane/BE/Profile_edit..Updated.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -26,6 +26,8 @@
                 @if (!Auth::check() || Auth::user()->role_id !== 1)
                     {{-- Search bar --}}
                     <form action="{{ route('posts.index') }}" method="get" class="search me-20 pt-3">
+
+                        {{-- <input type="search" name="search" value="{{ old('search',$search) }}" id="search" class="form-control" placeholder="search"> --}}
                         <input type="search" name="search" value="{{ isset($search) ? old('search',$search) : ''}}" id="search" class="form-control" placeholder="search">
                     </form>
 
