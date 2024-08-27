02e015e3e4490bf983e9c6233ee5b2502072a905
Yasuhiro WATANABE <info@nabeyasu.com>
2024-08-27 22:23:51 +0900

    [Hiro/BE/compoments_footer] - make footer color changed by logged-in user

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,13 @@
-@if(true)
-<!-- Users Footer -->
+@php
+    $footer_color = 'footer-color-user';
+
+    if (Auth::check() && Auth::user()->role_id === 1) {
+        $footer_color = 'footer-color-admin';
+    }
+@endphp
+
 <div class="footer">
-    <div class="container-fluid footer-user-container">
+    <div class="container-fluid footer-container {{ $footer_color }}">
         <div class=snslinks>
             <a href="#" class="text-white text-decoration-none">
                 <i class="fa-brands fa-facebook"></i>
@@ -15,50 +21,16 @@
         </div>
 
         <div class="text-white">
-            <h6>©︎2024 Omotenaship</h6>
+            <h6 class="m-0">©︎2024 Omotenaship</h6>
         </div>
 
-        <div class="footer-user-container">
+        <div class="d-flex">
             <a href="#" class="text-white text-decoration-none">
-                <h6>About</h6>
+                <h6 class="m-0">About</h6>
             </a>
             <a href="#" class="text-white text-decoration-none">
-                <h6>Contact</h6>
+                <h6 class="m-0 ms-3">Contact</h6>
             </a>
         </div>
     </div>
 </div>
-
-@elseif(false)
-{{-- if(Auth::user->role_id === 1) --}}
-<!-- Admin Footer -->
-<div class="footer">
-    <div class="container-fluid footer-container mt-auto">
-        <div class=snslinks>
-            <a href="#" class="text-white text-decoration-none">
-                <i class="fa-brands fa-facebook"></i>
-            </a>
-            <a href="#" class="text-white text-decoration-none">
-                <i class="fa-brands fa-instagram"></i>
-            </a>
-            <a href="#" class="text-white text-decoration-none">
-                <i class="fa-brands fa-x-twitter"></i>
-            </a>
-        </div>
-
-        <div class="text-white">
-            <h6>©︎2024 Omotenaship</h6>
-        </div>
-
-        <div class="footer-container">
-            <a href="#" class="text-white text-decoration-none">
-                <h6>About</h6>
-            </a>
-            <a href="#" class="text-white text-decoration-none">
-                <h6>Contact</h6>
-            </a>
-        </div>
-    </div>
-<div>
-
-@endif
