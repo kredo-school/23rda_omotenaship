aeeb9ca619823869275e38d75ae223ca6f86a069
Ruri Nogami <ruuuusan.1128@gmail.com>
2024-08-29 20:30:02 +0900

    remove search value

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -27,7 +27,7 @@
                     {{-- Search bar --}}
                     <form action="{{ route('posts.index') }}" method="get" class="search me-20 pt-3">
 
-                        <input type="search" name="search" value="{{ old('search',$search) }}" id="search" class="form-control" placeholder="search">
+                        <input type="search" name="search" value="" id="search" class="form-control" placeholder="search">
                     </form>
 
                 @endif
