52ab711601e0cdfab8609795487f097c7ccdcea1
Ruri Nogami <ruuuusan.1128@gmail.com>
2024-08-31 12:42:00 +0900

    remove something about index area for culture fix the categories area add route for each category

--- resources/views/components/category-icons.blade.php
+++ resources/views/components/category-icons.blade.php
@@ -6,13 +6,13 @@
 
     <div class="row mb-3 px-5">
         <div class="col d-flex justify-content-center">
-            <a href="#" class="icon-container">
+            <a href="{{ route('posts.index',['category' => 'volunteer']) }}" class="icon-container">
                 <img src="{{ asset('images/categories/1.png') }}" class="img-fluid">
                 <span class="hover-text">Volunteer</span>
             </a>
         </div>
         <div class="col d-flex justify-content-center">
-            <a href="#" class="icon-container">
+            <a href="{{ route('posts.index',['category' => 'event']) }}" class="icon-container">
                 <img src="{{ asset('images/categories/2.png') }}" class="img-fluid">
                 <span class="hover-text">Event</span>
             </a>
@@ -21,7 +21,7 @@
 
     <div class="row px-5 mt-5">
         <div class="col d-flex justify-content-center">
-            <a href="#" class="icon-container">
+            <a href="{{ route('posts.index',['category' => 'review']) }}" class="icon-container">
                 <img src="{{ asset('images/categories/3.png') }}" class="img-fluid">
                 <span class="hover-text">Review</span>
             </a>
