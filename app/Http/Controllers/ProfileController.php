6bfa58ffb24864a51e2ce213281185144d156ef8
DESKTOP-KOGVC0R\ii5aa <awawanov05@gmail.com>
2024-08-31 10:49:47 +0900

    Akane/BE/Profile_show..Updated.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -26,16 +26,29 @@ class ProfileController extends Controller
     public function show()
     {
         $profile = Auth::user()->profile;
-
         // dd($profile);
-        // $profile = Auth::user()->profile;  ←Auth
-        // $profile = $this->profile->where('user_id', 2)->first();
 
+        // languages
+        $languages = [
+            'en' => 'English' ,
+            'ja' => 'Japanese',
+            'fr' => 'French',
+            'de' => 'German' ,
+            'zh' => 'Chinese' ,
+            'ko' => 'Korean' ,
+        ];
+    
+        $profile->language 
+        = $languages[$profile->language] ?? $profile->language;   
+
+        
+        // pagination
         $posts = $profile->user->posts()->paginate(4);
 
         return view('profiles.show')
             ->with('profile', $profile)
             ->with('posts', $posts);
+        
     }
 
     // Edit
