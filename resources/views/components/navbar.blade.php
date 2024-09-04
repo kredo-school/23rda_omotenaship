f57b7282b437e4257e77049eccc9df6d025fbef2
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-04 16:38:30 +0900

    [Hiro/BE/components_navbar] - add collapse function to navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -22,12 +22,13 @@
         </a>
 
         {{-- Toggle Button --}}
-        {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent"> --}}
+        {{-- Collapse --}}
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 @if (!request()->is('login') && !request()->is('register'))
                     @if (!Auth::check() || Auth::user()->role_id !== 1)
@@ -100,6 +101,6 @@
                     @endif
                 @endif
             </ul>
-        {{-- </div> --}}
+        </div>
     </div>
 </nav>
