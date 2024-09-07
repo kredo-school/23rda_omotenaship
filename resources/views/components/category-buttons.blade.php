45d54f161dde9b3e34f2c7818ed22f56836e66e2
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-08 01:25:14 +0900

    [Hiro/category-buttons] - fix arrangement

--- resources/views/components/category-buttons.blade.php
+++ resources/views/components/category-buttons.blade.php
@@ -1,33 +1,41 @@
-<div class="containar components-icons">
-    {{-- Categories --}}
-    <div class="row text-center mb-4 mt-5">
-        <h5 class="text-bold underline-kurenai">Categories</h5>
+<div class="">
+    {{-- Heading --}}
+    <div class="row mb-4">
+        <h5 class="text-center text-bold">
+            <span class="px-2 heading-kurenai">Categories</span>
+        </h5>
     </div>
 
-    <div class="row mb-3 px-5">
-        <div class="col d-flex justify-content-center">
-            <a href="{{ route('posts.index',['category' => 'volunteer']) }}" class="icon-container">
+    <div class="row mb-2">
+        {{-- Volunteer --}}
+        <div class="col d-flex justify-content-end pe-3">
+            <a href="{{ route('posts.index', ['category' => 'volunteer']) }}" class="icon-container">
                 <img src="{{ asset('images/categories/1.png') }}" class="img-fluid">
                 <span class="hover-text">Volunteer</span>
             </a>
         </div>
-        <div class="col d-flex justify-content-center">
-            <a href="{{ route('posts.index',['category' => 'event']) }}" class="icon-container">
+
+        {{-- Event --}}
+        <div class="col d-flex justify-content-start ps-4">
+            <a href="{{ route('posts.index', ['category' => 'event']) }}" class="icon-container">
                 <img src="{{ asset('images/categories/2.png') }}" class="img-fluid">
                 <span class="hover-text">Event</span>
             </a>
         </div>
     </div>
 
-    <div class="row px-5 mt-5">
-        <div class="col d-flex justify-content-center">
-            <a href="{{ route('posts.index',['category' => 'review']) }}" class="icon-container">
+    <div class="row mt-5">
+        {{-- Review --}}
+        <div class="col d-flex justify-content-end pe-3">
+            <a href="{{ route('posts.index', ['category' => 'review']) }}" class="icon-container">
                 <img src="{{ asset('images/categories/3.png') }}" class="img-fluid">
                 <span class="hover-text">Review</span>
             </a>
         </div>
-        <div class="col d-flex justify-content-center">
-            <a href="{{ route('posts.index',['category' => 'culture']) }}" class="icon-container">
+
+        {{-- Culture --}}
+        <div class="col d-flex justify-content-start ps-3">
+            <a href="{{ route('posts.index', ['category' => 'culture']) }}" class="icon-container">
                 <img src="{{ asset('images/categories/4.png') }}" class="img-fluid">
                 <span class="hover-text">Culture</span>
             </a>
