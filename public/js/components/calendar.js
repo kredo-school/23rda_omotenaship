3bc144f4b4d996480e9efe0d566b3d9865bd064f
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-08 00:04:42 +0900

    [Hiro/calendar] - fix design

--- public/js/components/calendar.js
+++ public/js/components/calendar.js
@@ -1,20 +1,24 @@
 'use strict';
 
 {
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         var calendarEl = document.getElementById('calendar');
 
         var calendar = new FullCalendar.Calendar(calendarEl, {
             initialView: 'dayGridMonth',
-            dateClick: function(info) {
+            contentHeight: 'auto', // auto adjustment for height (prevent scroll)
+
+            dateClick: function (info) {
                 window.location.href = '/posts/calendar?date=' + info.dateStr;
                 document.getElementById('selected-date').innerText = selectedDate;
             },
+
             // hand pointer function
-            dayCellClassNames: function() {
-            return 'fc-day-pointer';
+            dayCellClassNames: function () {
+                return 'fc-day-pointer';
             }
         });
+
         calendar.render();
     });
 }
