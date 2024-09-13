2ef93bc5d3f8bee94c9fb58d671efeb2cbdbc58b
Ruri Nogami <ruuuusan.1128@gmail.com>
2024-09-13 21:49:22 +0900

    fix size add icon for info erea

--- resources/views/posts/select-category.blade.php
+++ resources/views/posts/select-category.blade.php
@@ -22,7 +22,8 @@
                     <div class="row">
                         <div class="col-md-4 col-lg-4">
                             {{-- Icon --}}
-                            <div class="d-flex justify-content-center align-items-center position-relative custom-logo-container">
+                            <div
+                                class="d-flex justify-content-center align-items-center position-relative custom-logo-container">
                                 <img src="{{ asset($logo_url) }}" alt="logo" class="custom-logo">
                                 <span class="overlay-text">user</span>
                             </div>
@@ -33,39 +34,39 @@
                             <div class="row h-100">
                                 {{-- Event --}}
                                 <div class="col-6 col-md-3 col-lg-3">
-                                {{-- <div class="col-6 col-lg-3"> --}}
+                                    {{-- <div class="col-6 col-lg-3"> --}}
                                     <label for="event" class="icon-position">Event</label>
                                     <a href="{{ route('posts.create', ['category_id' => 2]) }}"
-                                        class="d-flex justify-content-center align-items-center user-color custom-icon-container">
+                                        class="d-flex justify-content-center align-items-center user-color custom-icon-user">
                                         <img src="{{ asset('images/categories/2.png') }}">
                                     </a>
                                 </div>
                                 {{-- Volunteer --}}
                                 <div class="col-6 col-md-3 col-lg-3">
-                                {{-- <div class="col-6 col-lg-3"> --}}
-                                    <label for="volunteer" class="icon-position">Volunteer</label>
+                                    {{-- <div class="col-6 col-lg-3"> --}}
+                                    <label for="volunteer" class="ms-1 mb-3 mt-4">Volunteer</label>
                                     <a href="{{ route('posts.create', ['category_id' => 3]) }}"
-                                        class="d-flex justify-content-center align-items-center user-color custom-icon-container">
+                                        class="d-flex justify-content-center align-items-center user-color custom-icon-user">
                                         <img src="{{ asset('images/categories/1.png') }}">
                                     </a>
                                 </div>
 
                                 {{-- Review --}}
                                 <div class="col-6 col-md-3 col-lg-3">
-                                {{-- <div class="col-6 col-lg-3"> --}}
+                                    {{-- <div class="col-6 col-lg-3"> --}}
                                     <label for="review" class="icon-position">Review</label>
                                     <a href="{{ route('posts.create', ['category_id' => 1]) }}"
-                                        class="d-flex justify-content-center align-items-center user-color custom-icon-container">
+                                        class="d-flex justify-content-center align-items-center user-color custom-icon-user">
                                         <img src="{{ asset('images/categories/3.png') }}">
                                     </a>
                                 </div>
 
                                 {{-- Culture --}}
                                 <div class="col-6 col-md-3 col-lg-3">
-                                {{-- <div class="col-6 col-lg-3"> --}}
+                                    {{-- <div class="col-6 col-lg-3"> --}}
                                     <label for="culture" class="icon-position">Culture</label>
                                     <a href="{{ route('posts.create', ['category_id' => 4]) }}"
-                                        class="d-flex justify-content-center align-items-center user-color custom-icon-container">
+                                        class="d-flex justify-content-center align-items-center user-color custom-icon-user">
                                         <img src="{{ asset('images/categories/4.png') }}">
                                     </a>
                                 </div>
@@ -79,7 +80,8 @@
                     <div class="row">
                         <div class="col-md-4 col-lg-4">
                             {{-- Icon --}}
-                            <div class="d-flex justify-content-center align-items-center position-relative custom-logo-container">
+                            <div
+                                class="d-flex justify-content-center align-items-center position-relative custom-logo-container">
                                 <img src="{{ asset($logo_blue_url) }}" alt="logo" class="logo-organizer">
                                 <span class="overlay-text">Organizer</span>
                             </div>
@@ -91,16 +93,16 @@
                                 <div class="col-6 col-md-3 col-lg-3">
                                     <label for="event" class="icon-position icon-organizer">Event</label>
                                     <a href="{{ route('posts.create', ['category_id' => 5]) }}"
-                                        class="d-flex justify-content-center align-items-center">
+                                        class="d-flex justify-content-center align-items-center icon-filter">
                                         <img src="{{ asset('images/categories/2.png') }}">
                                     </a>
                                 </div>
 
                                 {{-- Volunteer --}}
                                 <div class="col-6 col-md-3 col-lg-3">
-                                    <label for="volunteer" class="icon-position icon-organizer">Volunteer</label>
+                                    <label for="volunteer" class="ms-1 mb-3 mt-4 icon-organizer">Volunteer</label>
                                     <a href="{{ route('posts.create', ['category_id' => 6]) }}"
-                                        class="d-flex justify-content-center align-items-center">
+                                        class="d-flex justify-content-center align-items-center icon-filter">
                                         <img src="{{ asset('images/categories/1.png') }}">
                                     </a>
                                 </div>
@@ -117,34 +119,75 @@
                     {{-- Header --}}
                     <h4>Information</h4>
 
-                    {{-- Info 1 --}}
-                    <div class="mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sed maxime quas at repellendus eos?
-                    </div>
 
-                    {{-- Info 2 --}}
-                    <div class="mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sed maxime quas at repellendus eos?
-                    </div>
+                    <div class="row">
+                        <span class="mb-2">User</span>
+                        {{-- Info Event --}}
+                        <div class="mb-3 d-flex align-items-start  custom-icon-user ms-4">
+                            <img src="{{ asset('images/categories/2.png') }}" class="me-3 info-icon-size">
+                            <div>
+                                <label for="event" class="d-block my-1">My Favarite Event</label>
+                                <span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                                </span>
+                            </div>
+                        </div>
 
-                    {{-- Info 3 --}}
-                    <div class="mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sed maxime quas at repellendus eos?
-                    </div>
+                        {{-- Info Volunteer --}}
+                        <div class="mb-3 d-flex align-items-start  custom-icon-user ms-4">
+                            <img src="{{ asset('images/categories/1.png') }}" class="me-3 info-icon-size">
+                            <div>
+                                <label for="volunteer" class="d-block my-1">Volunteer</label>
+                                <span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                                </span>
+                            </div>
+                        </div>
 
-                    {{-- Info 4 --}}
-                    <div class="mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sed maxime quas at repellendus eos?
-                    </div>
+                        {{-- Info Review --}}
+                        <div class="mb-3 d-flex align-items-start  custom-icon-user ms-4">
+                            <img src="{{ asset('images/categories/3.png') }}" class="me-3 info-icon-size">
+                            <div>
+                                <label for="Review" class="d-block my-1">My Experience Review</label>
+                                <span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                                </span>
+                            </div>
+                        </div>
 
-                    {{-- Info 5 --}}
-                    <div class="mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sed maxime quas at repellendus eos?
-                    </div>
+                        {{-- Info Culture--}}
+                        <div class="mb-3 d-flex align-items-start  custom-icon-user ms-4">
+                            <img src="{{ asset('images/categories/4.png') }}" class="me-3 info-icon-size">
+                            <div>
+                                <label for="Culture" class="d-block my-1">Japanese Culture</label>
+                                <span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                                </span>
+                            </div>
+                        </div>
+                        <hr>
+                        <span class="mb-2">Organizer</span>
+                        {{-- Info Organizer Event--}}
+                        <div class="mb-3 d-flex align-items-start  icon-filter ms-4">
+                            <img src="{{ asset('images/categories/2.png') }}" class="me-3 info-icon-size">
+                            <div>
+                                <label for="Culture" class="d-block my-1">Event</label>
+                                <span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                                </span>
+                            </div>
+                        </div>
 
-                    {{-- Info 6 --}}
-                    <div class="mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit sed maxime quas at repellendus eos?
+                         {{-- Info Organizer Volunteer--}}
+                         <div class="mb-3 d-flex align-items-start  icon-filter ms-4">
+                            <img src="{{ asset('images/categories/1.png') }}" class="me-3 info-icon-size">
+                            <div>
+                                <label for="Culture" class="d-block my-1">Event</label>
+                                <span>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing 
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
