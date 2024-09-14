47cd9365fb3eb0232ab4078d71739270b529c057
RIKO MAEZAWA <>
2024-09-14 16:53:42 +0900

    [Riko/FE/about_index] edit about.blade.php app.blade.php about.css web.php
    add&delete
    images(manekineko.png)

--- routes/web.php
+++ routes/web.php
@@ -176,10 +176,10 @@ Route::group(['middleware' => 'admin'], function () {
     });
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
