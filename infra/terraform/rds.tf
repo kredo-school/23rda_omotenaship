96b4bc59da474259662b740f7fe10a312939363c
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-18 05:13:35 +0900

    - Make RDS configuration in rds.tf

--- infra/terraform/rds.tf
+++ infra/terraform/rds.tf
@@ -1,21 +1,28 @@
-# # データベースサブネットグループの作成
-# resource "aws_db_subnet_group" "db_subnet_group" {
-#   name       = "db-subnet-group"
-#   subnet_ids = [aws_subnet.public_subnet.id]
-# }
+# データベースサブネットグループの作成
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name = "db-subnet-group"
+  subnet_ids = [
+    aws_subnet.subnet_a.id,
+    aws_subnet.subnet_c.id
+  ]
 
-# # RDS インスタンスの作成
-# resource "aws_db_instance" "mysql" {
-#   allocated_storage      = 20
-#   storage_type           = "gp2"
-#   engine                 = "mysql"
-#   engine_version         = "8.0"
-#   instance_class         = "db.t3.micro"
-#   db_name                = "laravel_db"
-#   username               = var.db_username
-#   password               = var.db_password
-#   parameter_group_name   = "default.mysql8.0"
-#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-#   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
-#   skip_final_snapshot    = true
-# }
+  tags = {
+    Name = "My DB Subnet Group"
+  }
+}
+
+# RDS インスタンスの作成
+resource "aws_db_instance" "mysql" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  db_name                = "laravel_db"
+  username               = var.db_username
+  password               = var.db_password
+  parameter_group_name   = "default.mysql8.0"
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+  skip_final_snapshot    = true
+}
