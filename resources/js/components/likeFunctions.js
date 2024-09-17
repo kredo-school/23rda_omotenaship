a1f8126d41e464ea678fdb19b5e3b1255a86c878
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-18 00:52:49 +0900

    - Fix likeFunctions.js and like.blade.php for like button

--- resources/js/components/likeFunctions.js
+++ resources/js/components/likeFunctions.js
@@ -11,7 +11,7 @@ export function updateLikeButtons() {
             if (mutation.type === 'childList') {
                 if (mutation.addedNodes.length > 0) {
                     mutation.addedNodes.forEach((node) => {
-                        if (node.nodeType === 1) {
+                        if (node.nodeType === 1) { // Element node
                             // console.log('Node added:', node);
                             addLikeButtons(node);
                         }
@@ -36,14 +36,10 @@ function addLikeButtons(container) {
         return;
     }
 
-    const likeButtons = container.querySelectorAll('[id^="like-button-"]');
+    const likeButtons = container.querySelectorAll('[class*="like-button-"]');
     likeButtons.forEach(likeButton => {
         // Get the post ID
         const postId = likeButton.dataset.postId;
-
-        // Get the like icon and like count
-        const likeIcon = container.querySelector(`#like-icon-${postId}`);
-        const likeCount = container.querySelector(`#like-count-${postId}`);
 
         const url = `/likes/${postId}/toggle`;
         const token = document.querySelector('meta[name="csrf-token"]').content;
@@ -69,23 +65,30 @@ function addLikeButtons(container) {
                 // Update the like icon and count
                 .then(data => {
                     // Change the color of the like icon
-                    likeIcon.classList.toggle('fa-regular');
-                    likeIcon.classList.toggle('fa-solid');
-                    likeIcon.classList.toggle('text-danger');
+                    const likeIcons = document.querySelectorAll(`.like-icon-${postId}`);
+                    likeIcons.forEach(likeIcon => {
+                        likeIcon.classList.toggle('fa-regular');
+                        likeIcon.classList.toggle('fa-solid');
+                        likeIcon.classList.toggle('text-danger');
+                    });
 
                     // Update the like count
-                    likeCount.textContent = data.postLikesCount;
+                    const likeCounts = document.querySelectorAll(`.like-count-${postId}`);
+                    likeCounts.forEach(likeCount => {
+                        // Update the like count
+                        likeCount.textContent = data.postLikesCount;
 
-                    // Change the color of the like count
-                    if (likeCount.textContent == 0) {
-                        if (!likeCount.classList.contains('text-white')) {
-                            likeCount.classList.add('text-white');
+                        // Change the color of the like count
+                        if (likeCount.textContent == 0) {
+                            if (!likeCount.classList.contains('text-white')) {
+                                likeCount.classList.add('text-white');
+                            }
+                        } else if (likeCount.textContent > 0) {
+                            if (likeCount.classList.contains('text-white')) {
+                                likeCount.classList.remove('text-white');
+                            }
                         }
-                    } else if (likeCount.textContent > 0) {
-                        if (likeCount.classList.contains('text-white')) {
-                            likeCount.classList.remove('text-white');
-                        }
-                    }
+                    });
                 })
                 // Catch any errors and log them to the console
                 .catch(error => {
