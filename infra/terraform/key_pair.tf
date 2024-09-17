b6de129f87ee2b66e36a06426f28961b9cfd1302
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-18 04:10:52 +0900

    - Make public key and set the path

--- infra/terraform/key_pair.tf
+++ infra/terraform/key_pair.tf
@@ -1,5 +1,6 @@
 # # キーペアの作成
-# resource "aws_key_pair" "deployer" {
-#   key_name   = "deployer-key"
-#   public_key = file("~/.ssh/deployer.pub") # 公開鍵のパス
-# }
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa_aws.pub") # 公開鍵のパス
+}
+# ```ssh-keygen -t rsa -b 4096 -C "your_email@example.com"``` で作成した公開鍵を指定
