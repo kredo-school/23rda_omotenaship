5d54ee3de34c127e7ecf5906a45b2e4fc0346333
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-18 05:12:56 +0900

    - Make network configuration in network.tf and subnet configuration in subnet.tf

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -8,13 +8,17 @@ resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0" # どこへのルーティングか
+    cidr_block = "0.0.0.0/0"                 # どこへのルーティングか
     gateway_id = aws_internet_gateway.igw.id # インターネットゲートウェイを指定
   }
 }
 
 # ルートテーブルの関連付け
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id # サブネットのIDを参照
+resource "aws_route_table_association" "public_assoc_a" {
+  subnet_id      = aws_subnet.subnet_a.id       # サブネットのIDを参照
+  route_table_id = aws_route_table.public_rt.id # ルートテーブルのIDを参照
+}
+resource "aws_route_table_association" "public_assoc_c" {
+  subnet_id      = aws_subnet.subnet_c.id       # サブネットのIDを参照
   route_table_id = aws_route_table.public_rt.id # ルートテーブルのIDを参照
 }
--- infra/terraform/subnet.tf
+++ infra/terraform/subnet.tf
@@ -1,7 +1,13 @@
-# サブネットの作成
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "subnet_a" {
   vpc_id                  = aws_vpc.main_vpc.id # VPCのIDを参照
   cidr_block              = "10.0.1.0/24" # CIDRブロック
   availability_zone       = "ap-northeast-1a" # 東京リージョンのAZを指定
+  map_public_ip_on_launch = true # インスタンス起動時にパブリックIPを自動割り当て
+}
+
+resource "aws_subnet" "subnet_c" {
+  vpc_id                  = aws_vpc.main_vpc.id # VPCのIDを参照
+  cidr_block              = "10.0.2.0/24" # CIDRブロック
+  availability_zone       = "ap-northeast-1c" # 東京リージョンのAZを指定
   map_public_ip_on_launch = true # インスタンス起動時にパブリックIPを自動割り当て
 }
