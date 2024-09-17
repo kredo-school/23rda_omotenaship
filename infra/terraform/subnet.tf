6bc351bde5eb2bec903c5131d64336d64cd0d289
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-18 03:35:41 +0900

    - Make subnet configuration in subnet.tf

--- infra/terraform/subnet.tf
+++ infra/terraform/subnet.tf
@@ -1,7 +1,7 @@
-# # サブネットの作成
-# resource "aws_subnet" "public_subnet" {
-#   vpc_id                  = aws_vpc.main_vpc.id
-#   cidr_block              = "10.0.1.0/24"
-#   availability_zone       = "ap-northeast-1a"
-#   map_public_ip_on_launch = true
-# }
+# サブネットの作成
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id # VPCのIDを参照
+  cidr_block              = "10.0.1.0/24" # CIDRブロック
+  availability_zone       = "ap-northeast-1a" # 東京リージョンのAZを指定
+  map_public_ip_on_launch = true # インスタンス起動時にパブリックIPを自動割り当て
+}
