da44721e3d687ff07c1eef82116a9acc1ed6574f
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-17 19:04:38 +0900

    Add variables for database credentials in Terraform configuration

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true # 機密情報であることを示す
+}
