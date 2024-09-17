de627efaf28d14ce58693553f9133165f380edd4
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-17 19:05:07 +0900

    Add Terraform outputs for EC2 instance public IP and RDS endpoint

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "rds_endpoint" {
+  description = "Endpoint of the RDS instance"
+  value       = aws_db_instance.mysql.address
+}
