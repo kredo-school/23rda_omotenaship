5a523b09ae1dec316f13d1afc99f84e30aba5b26
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-19 13:24:42 +0900

    Add a note

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "ec2_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web.public_ip # EC2インスタンスのパブリックIPアドレス (54.249.94.9)
+  value       = aws_instance.web.public_ip # EC2インスタンスのパブリックIPアドレス (http://54.249.94.9)
 }
 
 output "rds_endpoint" {
