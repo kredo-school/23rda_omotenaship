3ab7a7ae8e320c3b73aaade8d1941495cba3d1c9
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-20 14:08:08 +0900

    Refactor unzip_zip.sh script to improve file handling and integrity check

--- infra/scripts/unzip_zip.sh
+++ infra/scripts/unzip_zip.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -ex
 
+# ZIPファイルの整合性を確認する
+echo "Checking the integrity of laravel-app.zip..."
+zip -T /home/ec2-user/laravel-app.zip
+
 echo "Creating laravel-app directory..."
 mkdir -p /home/ec2-user/laravel-app
 
+echo "Changing ownership and permissions..."
+sudo chown -R ec2-user:ec2-user /home/ec2-user/laravel-app
+sudo chmod -R 755 /home/ec2-user/laravel-app
+
 echo "Unzipping laravel-app.zip..."
-unzip /home/ec2-user/laravel-app.zip -d /home/ec2-user/laravel-app
+unzip -o /home/ec2-user/laravel-app.zip -d /home/ec2-user/laravel-app
 
 echo "Removing laravel-app.zip..."
 rm /home/ec2-user/laravel-app.zip
+
+echo "Unzip completed successfully."
