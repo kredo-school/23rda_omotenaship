4618f1a08804ca72187aeccd595e97549461e4db
Yasuhiro WATANABE <info@nabeyasu.com>
2024-09-20 13:58:58 +0900

    Refactor zip_laravel.sh script to improve file handling and integrity check

--- infra/scripts/zip_laravel.sh
+++ infra/scripts/zip_laravel.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -ex
 
-zip -r "${1}/laravel-app.zip" ../../ -x '../../infra/terraform/*'
-ls -l "${1}/laravel-app.zip"
+# プロジェクトのルートディレクトリに移動
+PROJECT_ROOT="$(dirname "$1")/../.."
+cd "$PROJECT_ROOT"
+
+# 既存のファイルを削除
+echo "Removing existing laravel-app.zip..."
+rm -f "${1}/infra/terraform/laravel-app.zip"
+
+# .git ディレクトリと terraform ディレクトリを除外して zip を作成
+echo "Creating laravel-app.zip..."
+zip -r "${1}/infra/terraform/laravel-app.zip" . -x "${1}/infra/terraform/*"
+
+# 作成した zip ファイルの整合性を確認
+echo "Checking the integrity of laravel-app.zip..."
+zip -T "${1}/infra/terraform/laravel-app.zip"
+
 echo 'Zip file created successfully'
